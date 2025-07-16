76ef85377147c41a93d5b07ff4671263da389e30
belatoledo <it.amancio@gmail.com>
2025-07-16 04:46:53 -0300

    refactor(data): create dedicated api module for markdown data access

--- src/lib/posts-api.ts
+++ src/lib/posts-api.ts
@@ -1,3 +1,4 @@
+import { Post, PostPreview } from '@/domain/posts/post';
 import fs from 'fs';
 import path from 'path';
 import matter from 'gray-matter';
@@ -6,63 +7,51 @@ import html from 'remark-html';
 
 const postsDirectory = path.join(process.cwd(), '_posts');
 
-export interface PostMetadata {
-  id: string;
-  title: string;
-  date: string;
-  author: string;
-  excerpt: string;
-  coverImage: string;
-}
-
-export function getSortedPostsData(): PostMetadata[] {
-    const fileNames = fs.readdirSync(postsDirectory);
-    const allPostsData = fileNames.map((fileName) => {
+export function getAllPosts(): PostPreview[] {
+  const fileNames = fs.readdirSync(postsDirectory);
+  const allPostsData = fileNames.map((fileName) => {
     const id = fileName.replace(/\.md$/, '');
     const fullPath = path.join(postsDirectory, fileName);
     const fileContents = fs.readFileSync(fullPath, 'utf8');
-
     const matterResult = matter(fileContents);
+
     return {
       id,
-      ...(matterResult.data as Omit<PostMetadata, 'id'>),
+      title: matterResult.data.title,
+      date: matterResult.data.date,
+      author: matterResult.data.author,
+      coverImage: matterResult.data.coverImage,
+      excerpt: matterResult.data.excerpt,
     };
   });
 
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
+  return allPostsData.sort((a, b) => (a.date < b.date ? 1 : -1));
 }
 
-
-export async function getPostData(id: string) {
+export async function getPostById(id: string): Promise<Post> {
   const fullPath = path.join(postsDirectory, `${id}.md`);
   const fileContents = fs.readFileSync(fullPath, 'utf8');
-
   const matterResult = matter(fileContents);
 
   const processedContent = await remark()
     .use(html)
     .process(matterResult.content);
   const contentHtml = processedContent.toString();
+
   return {
     id,
     contentHtml,
-    ...(matterResult.data as Omit<PostMetadata, 'id'>),
+    title: matterResult.data.title,
+    date: matterResult.data.date,
+    author: matterResult.data.author,
+    coverImage: matterResult.data.coverImage,
+    excerpt: matterResult.data.excerpt,
   };
 }
 
 export function getAllPostIds() {
   const fileNames = fs.readdirSync(postsDirectory);
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
-    };
-  });
+  return fileNames.map((fileName) => ({
+    id: fileName.replace(/\.md$/, ''),
+  }));
 }
